4f38657c3d0ff72b5f06e54fc427a271597a780d
odayeong1 <odayeong1@gmail.com>
2025-12-17 23:56:55 +0900

    Update moodiary_app.py

--- moodiary_app.py
+++ moodiary_app.py
@@ -165,7 +165,7 @@ def apply_custom_css():
         }}
         .animated-title {{ font-size: 3.5rem !important; font-weight: 800; animation: color-shift 5s ease-in-out infinite alternate; }}
 
-        header {{visibility: hidden;}} 
+        header {{visibility: visible;}} 
         footer {{visibility: hidden;}}
         </style>
     """
@@ -747,3 +747,4 @@ def page_happy_storage(sh):
 if st.session_state.logged_in: main_app()
 elif st.session_state.page == "intro": intro_page()
 else: login_page()
+
